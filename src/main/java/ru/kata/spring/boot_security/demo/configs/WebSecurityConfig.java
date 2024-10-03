268e5e168b2aee66059d499d40a4ae009a13377f
ZhulkovNikolay <wed10@yandex.ru>
2024-10-03 23:43:05 +0300

    DaoAuth

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -1,49 +1,81 @@
 package ru.kata.spring.boot_security.demo.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final SuccessUserHandler successUserHandler;
+    // private final SuccessUserHandler successUserHandler;
+    //  public WebSecurityConfig(SuccessUserHandler successUserHandler) {
+    //      this.successUserHandler = successUserHandler;
+    // }
 
-    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
-        this.successUserHandler = successUserHandler;
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
-                .anyRequest().authenticated()
+                //.antMatchers("/", "/index").permitAll()
+                .antMatchers("/authenticated/**").authenticated()
+                //роль и ауторити по сути одно и то же, разница лишь в префиксе, который дописываетс крытно Спринг
+                .antMatchers("/only_for_admins/**").hasRole("ADMIN")
+                .antMatchers("/read_profile/**").hasAuthority("READ_PROFILE")
+                //.anyRequest().authenticated()
                 .and()
-                .formLogin().successHandler(successUserHandler)
-                .permitAll()
+                //если пользователь прошел аутентификацию, то мы должны выполнить ему преднастройку
+                //.formLogin().successHandler(successUserHandler)
+                .formLogin()
                 .and()
-                .logout()
-                .permitAll();
+                //.permitAll()
+                //.and()
+                //.httpBasic();
+                .logout().logoutSuccessUrl("/");
+        //  .permitAll();
     }
 
-    // аутентификация inMemory
+
+    //  @Bean
+    //подключаемся к БД и от туда берем наших Юзеров
+//    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        return jdbcUserDetailsManager;
+//    }
+
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("user")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
+
+    //Данный метод проверяет существует ли такой пользователь или нет
+    //и если да, то кладет его в секюрити контекст
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder()); //прогоняет через шифрование
+        daoAuthenticationProvider.setUserDetailsService(userService); //предоставляет самих юзеров
+        return daoAuthenticationProvider;
+    }
+
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -0,0 +1,58 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.entities.Role;
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements UserDetailsService {
+    //его задача по имени пользователя предоставить самого юзера
+    //поэтому для начала нам нужен доступ к самому юзер репозиторию
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //делаем обертку вокруг репозитория чтобы напрямую к нему не обращаться
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+
+    //нам дадут какое-то имя пользователя
+    //этот метод вернет самого Юзера по полученному имени
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username); //достаем пользователя из базы по имени
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("user '%s' not found", username));
+        }
+        //Спрингу нужно знать только имя, пароль и право джоступа. Этим заведует UserDetails
+        //Наша задача нашего Юзера в БД привети к типу Юзера спрингова из UserDetails
+        //мы возвращаем спрингового юзера и внутрь кладем нашего
+        return new org.springframework.security.core.userdetails
+                .User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    //здесь подается список ролей из Юзера и мы должны преобразовать пачку ролей в пачку Authorities
+    //нужно для метода выше
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+    }
+
+}
