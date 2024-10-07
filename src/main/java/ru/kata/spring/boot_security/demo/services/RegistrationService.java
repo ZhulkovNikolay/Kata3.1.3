dbc0d00f9be8f14b34c0a7726d62f9a25222d821
ZhulkovNikolay <wed10@yandex.ru>
2024-10-07 22:53:23 +0300

    EncryptRegistration

--- src/main/java/ru/kata/spring/boot_security/demo/services/RegistrationService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entities.User;
@@ -10,14 +11,18 @@ import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 public class RegistrationService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(UserRepository userRepository) {
+    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(User user) {
+        String encryptResult = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptResult);
         userRepository.save(user);
     }
 
